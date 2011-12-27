1d402a5ae5884b6132a0b935989faa3666132b33
gerry <gerry@moodlevm.(none)>
2011-12-27 22:48:07 +0800

    Add command to script so that it makes the downloaded script executable

--- grabGitNewDirectory.sh
+++ grabGitNewDirectory.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+sudo read;
 wget "http://git.kernel.org/?p=git/git.git;a=blob_plain;f=contrib/workdir/git-new-workdir;hb=HEAD" -O - | sudo tee /usr/local/bin/git-new-workdir
+sudo chmod u+x /usr/local/bin/git-new-workdir
