bfc0c8bc638507e7e8feddc897c543d03cc648a0
Gerard (Gerry) Caulfield <gerry@moodle.com>
2012-01-23 13:15:14 +0800

    Adding a github remote branches update script

---
+++ updateGithubMoodle.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+versionTypes=(int pro)
+versions=(m 22 21 20 19)
+remote=$1
+for versionType in "${versionTypes[@]}"; do
+	echo "git fetch r_$versionType"
+	#git fetch r_$versionType
+	for version in "${versions[@]}"; do
+		if [ "$version" = "m" ]; then
+			fullVersionName="master"
+		else
+			fullVersionName="MOODLE_${version}_STABLE"
+		fi
+
+		echo "git push -f $remote r_$versionType/$fullVersionName:$fullVersionName"
+		#git push -f $remote r_$versionType/$fullVersionName:$fullVersionName
+	done
+done
+
